eb5e6dff60c5143c70ca91d8b3d94faa5f556842
henry blas <hblas2014@gmail.com>
2023-04-24 22:36:31 -0500

    se realizo ejemplos de variables en java script

---
+++ app.js
@@ -0,0 +1,37 @@
+// Ejemplos de variables STRING
+ 
+ let miNombre = "Henry" ; 
+ let miApellido = "Blas" ;
+ let curso = "Programacion Java Script" ;
+
+ console.log(miNombre);
+ console.log(miApellido);
+ console.log(curso); 
+
+ // Ejemplos de variables NUMBER
+
+ let miEdad = 43 ; 
+ let añonacimiento = 1980 ;
+ let mesnacimiento = 10 ;
+
+  // Ejemplos de variables BOOLEAN
+
+ let  verdadero = true ; 
+ let  falso = false ;
+ let  miVariable = true ;
+
+ // Ejemplos de variables NULL
+
+ let  hobbie = null ; 
+ let  apellido2 = null ;
+ let  emai = null ;
+
+ // Ejemplos de variables UNDEFINED
+
+ let  nombre; 
+ let  apellido;
+ let  edad;
+
+ console.log(nombre);
+ console.log(apellido);
+ console.log(edad);
